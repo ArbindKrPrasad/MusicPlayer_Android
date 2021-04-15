985981f5e7f40760d54838e8cf128d93f366fe92
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-04-15 11:01:07 +0530

    Running perfectly but the next and previous is not working

--- app/src/main/java/com/arbind/musicplayer/PlayerFragment.java
+++ app/src/main/java/com/arbind/musicplayer/PlayerFragment.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -149,6 +150,28 @@ public class PlayerFragment extends Fragment {
                 }
             }
         });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    int changeBar = seekBar.getProgress();
+                    seekBar.setProgress(changeBar);
+                    mediaPlayer.seekTo(changeBar);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         return rootView;
     }
 
